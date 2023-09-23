1d9307bc1f0577e4f21a72973c176119f90e0862
Tommy <tommy128works@gmail.com>
2023-09-23 15:50:30 -0400

    build: create React project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <p>default paragraph</p>
+      <p>default paragraph</p>
+    </div>
+  );
+}
+
+export default App;
