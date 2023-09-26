ba179fd8176b821556f34fede88f5bb2c5f73aad
Tommy <tommy128works@gmail.com>
2023-09-26 16:42:56 -0400

    feat: download icons for NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"; // need to change to scss file
-import HomePage from "./components/HomePage.tsx";
+import HomePage from "./components/HomePage.jsx";
+import NavBar from "./components/NavBar.jsx"
 
 function App() {
   return (
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "../styles/NavBar.scss";
+
+import menuIcon from "../assets/icons/menu_icon.svg";
+import homeIcon from "../assets/icons/home_icon.svg";
+import searchIcon from "../assets/icons/search_icon.svg";
+import shoppingCartIcon from "../assets/icons/shopping_cart_icon.svg";
+
+const NavBar = () => {
+
+  return (
+    <div id="nav-bar" >
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+
+
+
+    </div>
+  )
+}
+
+export default NavBar;
